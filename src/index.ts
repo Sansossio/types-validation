d0556f7e79842ad3e8df56974c7aea30be887294
Sansossio <juliosansossio@gmail.com>
2020-05-14 21:58:00 +0200

    Delete boilerplate index file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { Injectable, InjectorApp } from 'dependency-injection-implementation'
-
-class B {
-  value = 1
-}
-
-@Injectable()
-class C {
-  constructor (
-    readonly b: B
-  ) {}
-}
-
-const app = InjectorApp.create([C, B])
-const c = app.get(C)
-
-console.log(c.b.value)
+console.log('hello world')

0e7dab171601428366cb10030072e51402f34001
Sansossio <juliosansossio@gmail.com>
2020-05-15 19:07:30 +0200

    Update filename

--- src/validators/is-string/is-string.validator.ts
+++ src/validators/is-string/is-string.validator.ts
@@ -1,5 +1,5 @@
 import { ValidationResponse } from '../../types/validator/validator.types'
-import { ValidatorsErrorMessages } from '../error_messages'
+import { ValidatorsErrorMessages } from '../error-messages'
 
 export function isStringValidator (value: any): ValidationResponse {
   const valid = typeof value === 'string'

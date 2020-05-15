c05cb5ca8b8b294974ede09ad164298a803b2d87
Sansossio <juliosansossio@gmail.com>
2020-05-15 18:24:56 +0200

    Save property key

--- src/decorators/is-string/is-string.decorator.ts
+++ src/decorators/is-string/is-string.decorator.ts
@@ -2,13 +2,16 @@ import 'reflect-metadata'
 import { MethodProperty } from '../../types/method-property/method-property.types'
 import { isStringValidator } from '../../validators/is-string/is-string.validator'
 import { setProperty } from '../../utils/set-property/set-property.utils'
+import { getParameters } from '../../utils/get-parameters/get-parameters.utils'
 
 export function IsString () {
   return function (target: Object, propertyKey: string | symbol, parameterIndex: number) {
     const registerProperty: MethodProperty = {
       index: parameterIndex,
-      validator: isStringValidator
+      validator: isStringValidator,
+      key: getParameters(target[propertyKey])[parameterIndex]
     }
+    const key = Reflect.getMetadata('design:paramtypes', target, propertyKey)
     setProperty(registerProperty, target, propertyKey)
   }
 }

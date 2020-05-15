7ce1e7eb947d312d0419fdeb7d11f44f867a0a3f
Sansossio <juliosansossio@gmail.com>
2020-05-15 19:21:01 +0200

    Validation exception

---
+++ src/errors/validation-exception.error.ts
@@ -0,0 +1,15 @@
+import { ValidatorResponseError } from '../types/validator/validator.types'
+
+export class ValidationException extends Error {
+  message = `Invalid parameters \n ${this.parseErrors()}`
+  constructor (
+    readonly errors: ValidatorResponseError[]
+  ) {
+    super()
+    Object.setPrototypeOf(this, ValidationException.prototype)
+  }
+
+  private parseErrors () {
+    return JSON.stringify(this.errors, null ,2)
+  }
+}

20467059096bb25e4ac77e1a505078a581d7e441
Sansossio <juliosansossio@gmail.com>
2020-05-16 21:43:13 +0200

    Fix test

---
+++ src/utils/get-function-parameters/get-function-parameters.utils.spec.ts
@@ -0,0 +1,26 @@
+import { getFunctionParamsName } from '../get-function-params-name/get-function-params-name.utils'
+
+describe('Get parameters', () => {
+  it('should return parameters name from a function', () => {
+    function a (b,c) { console.log(b, c) }
+    expect(getFunctionParamsName(a)).toEqual(['b', 'c'])
+  })
+
+  it('should return parameters name from a class method', () => {
+    class Test {
+      a (b, c) {
+        console.log(b, c)
+      }
+    }
+    expect(getFunctionParamsName(new Test().a)).toEqual(['b', 'c'])
+  })
+
+  it('should return parameters name from a class static method', () => {
+    class Test {
+      static a (b, c) {
+        console.log(b, c)
+      }
+    }
+    expect(getFunctionParamsName(Test.a)).toEqual(['b', 'c'])
+  })
+})

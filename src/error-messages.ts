98e6cd5eccbd79d14c2ef64c6e933f9dc4ca76dc
Sansossio <juliosansossio@gmail.com>
2020-05-17 19:39:34 +0200

    Add validate schema message

--- src/error-messages.ts
+++ src/error-messages.ts
@@ -1,4 +1,5 @@
 export const ValidatorsErrorMessages = {
   IS_STRING: 'Property "{key}" must be a string',
-  IS_NUMBER: 'Property "{key}" must be a number'
+  IS_NUMBER: 'Property "{key}" must be a number',
+  VALIDATE_SCHEMA: 'Schema "{key}" is not valid'
 }

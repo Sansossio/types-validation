fd8c35827dd5537e3601ce27896139c76a427098
Sansossio <juliosansossio@gmail.com>
2020-05-18 04:46:02 +0200

    Required params

--- src/types/method-property/method-property.types.ts
+++ src/types/method-property/method-property.types.ts
@@ -1,7 +1,9 @@
 import { MethodPropertyValidator } from './method-property.validator.types'
+import { DecoratorParams } from '../decorator-params/decorator-params.types'
 
 export type MethodProperty = {
   validator: MethodPropertyValidator
   index: number
   key: string
+  params: DecoratorParams
 }

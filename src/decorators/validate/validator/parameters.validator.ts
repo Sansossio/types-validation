397c3809a4567aa2bd7ab197f9a4a7f39d309c2f
Sansossio <juliosansossio@gmail.com>
2020-05-18 05:20:43 +0200

    Don't validate each

--- src/decorators/validate/validator/parameters.validator.ts
+++ src/decorators/validate/validator/parameters.validator.ts
@@ -3,28 +3,23 @@ import { ValidatorResponseError } from '../../../types/validator/validator-error
 import { parseMessage } from '../../../utils/parse-message/parse-message.utils'
 import { ValidationException } from '../../../errors/validation-exception.error'
 import { parseInternalMessages } from './utils/parse-internal-messages.utils'
-import { castArray } from '../../../utils/cast-array/cast-array.utils'
 
 export function validateParameters (properties: MethodProperty[], args: IArguments | any[]): void {
   const errors: ValidatorResponseError[] = []
   for (const property of properties) {
-    const baseValue = args[property.index]
-    const values = castArray(baseValue)
-    for (let i = 0; i < values.length; i++) {
-      const value = values[i]
-      const key = Array.isArray(baseValue) ? `${property.key}[${i}]` : property.key
-      const {
-        valid,
-        description,
-        messages = []
-      } = property.validator(key, value)
-      if (!valid) {
-        errors.push({
-          property: key,
-          description: parseMessage(description, { key }),
-          messages: parseInternalMessages(messages)
-        })
-      }
+    const value = args[property.index]
+    const { key } = property
+    const {
+      valid,
+      description,
+      messages = []
+    } = property.validator(key, value)
+    if (!valid) {
+      errors.push({
+        property: key,
+        description: parseMessage(description, { key }),
+        messages: parseInternalMessages(messages)
+      })
     }
   }
   if (errors.length > 0) {

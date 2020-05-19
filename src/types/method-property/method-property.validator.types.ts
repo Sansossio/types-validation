99ef2b4df899b95e24b3d45e9491d13d58e4e96d
Sansossio <juliosansossio@gmail.com>
2020-05-19 04:57:41 +0200

    Remove symbol

--- src/types/method-property/method-property.validator.types.ts
+++ src/types/method-property/method-property.validator.types.ts
@@ -1,3 +1,3 @@
 import { ValidationResponse } from '../validator/validator.types'
 
-export type MethodPropertyValidator = (key: string | symbol, value: any) => ValidationResponse
+export type MethodPropertyValidator = (key: string, value: any) => ValidationResponse

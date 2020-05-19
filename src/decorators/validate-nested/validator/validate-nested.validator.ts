4974d920ba6fd552a752b7866fcd7468955e0206
Sansossio <juliosansossio@gmail.com>
2020-05-19 04:53:20 +0200

    Validate each

--- src/decorators/validate-nested/validator/validate-nested.validator.ts
+++ src/decorators/validate-nested/validator/validate-nested.validator.ts
@@ -1,34 +1,37 @@
 import { ValidationResponse } from '../../../types/validator/validator.types'
 import { ValidatorsErrorMessages } from '../../../error-messages'
 import { getClassParameters } from '../../../utils/get-class-parameters/get-class-parameters.utils'
+import { ValidateSchemaOptions } from '../validate-nested.options'
+import { castArray } from '../../../utils/cast-array/cast-array.utils'
+import { validateNestedCallValidators } from './utils/validate-nested.call-validators.utils'
 
 const description = ValidatorsErrorMessages.VALIDATE_SCHEMA
 
-export function validateNestedValidator (key: string, type: any) {
-  return function (value: any): ValidationResponse {
-    let valid = true
+export function validateNestedValidator (key: string, options: ValidateSchemaOptions) {
+  return function (_: string, value: any): ValidationResponse {
     const messages: ValidationResponse[] = []
-    const properties = getClassParameters(type)
+    const properties = getClassParameters(options.type)
     if (properties.length === 0) {
-      valid = false
       return {
         key,
-        valid,
-        description
+        valid: false,
+        description,
+        messages
       }
     }
-    for (const property of properties) {
-      const { key, validator } = property
-      const result = validator(key, value?.[key])
-      if (!result.valid) {
-        valid = false
-        messages.push(result)
+    if (options.each) {
+      const values = castArray(value)
+      let i = 0
+      for (const val of values) {
+        validateNestedCallValidators(properties, messages, val, i++)
       }
+    } else {
+      validateNestedCallValidators(properties, messages, value)
     }
     return {
       key,
-      valid,
       description,
+      valid: messages.length === 0,
       messages: messages
     }
   }

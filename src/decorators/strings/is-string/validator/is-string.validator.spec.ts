169146c3d7be63eb782fb50431689af0c68095b2
Sansossio <juliosansossio@gmail.com>
2020-05-21 09:42:52 +0200

    Remove magic string

--- src/decorators/strings/is-string/validator/is-string.validator.spec.ts
+++ src/decorators/strings/is-string/validator/is-string.validator.spec.ts
@@ -1,14 +1,15 @@
 import { isStringValidator } from './is-string.validator'
 
 describe('Validator - IsString', () => {
+  const key = ''
   it('should return false when value is not a string', () => {
-    expect(isStringValidator('', null).valid).toEqual(false)
-    expect(isStringValidator('', undefined).valid).toEqual(false)
-    expect(isStringValidator('', 0).valid).toEqual(false)
-    expect(isStringValidator('', {}).valid).toEqual(false)
+    expect(isStringValidator(key, null).valid).toEqual(false)
+    expect(isStringValidator(key, undefined).valid).toEqual(false)
+    expect(isStringValidator(key, 0).valid).toEqual(false)
+    expect(isStringValidator(key, {}).valid).toEqual(false)
   })
 
   it('should return true when value is a string', () => {
-    expect(isStringValidator('', '').valid).toEqual(true)
+    expect(isStringValidator(key, '').valid).toEqual(true)
   })
 })

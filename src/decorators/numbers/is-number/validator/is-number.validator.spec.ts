b15961adb697caec7cce0c49bd1dd7d37dd412b3
Sansossio <juliosansossio@gmail.com>
2020-05-21 09:42:50 +0200

    Remove magic string

--- src/decorators/numbers/is-number/validator/is-number.validator.spec.ts
+++ src/decorators/numbers/is-number/validator/is-number.validator.spec.ts
@@ -1,14 +1,15 @@
 import { isNumberValidator } from './is-number.validator'
 
 describe('Validator - IsNumber', () => {
+  const key = ''
   it('should return false when value is not a number', () => {
-    expect(isNumberValidator('', null).valid).toEqual(false)
-    expect(isNumberValidator('', undefined).valid).toEqual(false)
-    expect(isNumberValidator('', '').valid).toEqual(false)
-    expect(isNumberValidator('', {}).valid).toEqual(false)
+    expect(isNumberValidator(key, null).valid).toEqual(false)
+    expect(isNumberValidator(key, undefined).valid).toEqual(false)
+    expect(isNumberValidator(key, '').valid).toEqual(false)
+    expect(isNumberValidator(key, {}).valid).toEqual(false)
   })
 
   it('should return true when value is a number', () => {
-    expect(isNumberValidator('', 0).valid).toEqual(true)
+    expect(isNumberValidator(key, 0).valid).toEqual(true)
   })
 })

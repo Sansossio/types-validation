170aa293d087e44c97bc92860074649a13eb7a68
Sansossio <juliosansossio@gmail.com>
2020-05-24 13:31:19 +0200

    Const order

--- src/error-messages.ts
+++ src/error-messages.ts
@@ -1,8 +1,8 @@
 export const ValidatorsErrorMessages = {
+  IS_NOT_EMPTY: 'Property "{key}" must be not empty',
   IS_STRING: 'Property "{key}" must be a string',
   IS_NUMBER: 'Property "{key}" must be a number',
   IS_POSITIVE: 'Property "{key}" must be a positive number',
   IS_ARRAY: 'Property "{key}" must be an array',
-  VALIDATE_SCHEMA: 'Schema "{key}" is not valid',
-  IS_NOT_EMPTY: 'Property "{key}" must be not empty'
+  VALIDATE_SCHEMA: 'Schema "{key}" is not valid'
 }
